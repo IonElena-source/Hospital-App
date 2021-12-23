c72d90c39106cd767863b7aca0d12a7ac5e6bd14
IonElena-source <62117703+IonElena-source@users.noreply.github.com>
2021-12-24 01:40:05 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-    mongoURL:"mongodb+srv://ion_elena_cristina:C12345678@cluster0.bsq1q.mongodb.net/hr?retryWrites=true&w=majority",
+    mongoURL:"mongodb+srv://<user>:<password>@cluster0.bsq1q.mongodb.net/<database>?retryWrites=true&w=majority",
     secret:"yoursecret"
 }
